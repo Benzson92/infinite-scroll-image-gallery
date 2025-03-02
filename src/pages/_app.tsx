8a8b469ca3d7009b6e26c7645074a964f8185c2c
Benzson92 <benzson.v@gmail.com>
2025-03-02 20:08:18 +0700

    Integrate React Query
    Integrate React Query for data fetching and add React Query Devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// ✅ Structural Import Grouping
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// ✅ React Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import "@/styles/globals.css"; // Import global styles
+
+const queryClient = new QueryClient();
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
