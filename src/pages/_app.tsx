cdc6f874d160b94a5771f03c48907f0ed30b3818
Benzson92 <benzson.v@gmail.com>
2025-03-02 22:15:04 +0700

    Add toaster
    Add Sonner toaster component and integrate next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-// ✅ Structural Import Grouping
 import { AppProps } from "next/app";
 
-// ✅ React Query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css"; // Import global styles
 
 const queryClient = new QueryClient();
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <Toaster richColors position="top-center" />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
