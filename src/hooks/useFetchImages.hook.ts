a80a8f43c57edfa61dbe853bc864ee0f9a2d2c6b
Benzson92 <benzson.v@gmail.com>
2025-03-02 20:07:25 +0700

    Add custom hook for fetching images with pagination

---
+++ src/hooks/useFetchImages.hook.ts
@@ -0,0 +1,50 @@
+// ✅ Structural Import Grouping
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+import { GetPicsumPhotoAPIResponseDTO } from "@/models/dto/GetPicsumPhotoAPI.dto";
+
+// // ✅ Define the image type
+// type Image = {
+//   id: string;
+//   download_url: string;
+//   author: string;
+// };
+
+// interface ImageDTO {
+//   id: string;
+//   author: string;
+//   width: number;
+//   height: number;
+//   url: string;
+//   download_url: string;
+// }
+
+// // ✅ Fetch images with pagination
+// const fetchImages = async ({ pageParam = 1 }): Promise<ImageDTO[]> => {
+//   const { data } = await axios.get(
+//     `https://picsum.photos/v2/list?page=${pageParam}&limit=9`
+//   );
+//   return data;
+// };
+
+// ✅ Fetch images with pagination and limit
+const fetchImages = async ({
+  pageParam = 1,
+  limit = 9,
+}): Promise<GetPicsumPhotoAPIResponseDTO[]> => {
+  const { data } = await axios.get<GetPicsumPhotoAPIResponseDTO[]>(
+    `https://picsum.photos/v2/list?page=${pageParam}&limit=${limit}`
+  );
+  return data;
+};
+
+// ✅ Custom Hook for Infinite Scroll
+export const useFetchImages = () => {
+  return useInfiniteQuery({
+    queryKey: ["images"],
+    queryFn: fetchImages,
+    initialPageParam: 1, // Start from page 1
+    getNextPageParam: (_, pages) => pages.length + 1, // Increment page number
+  });
+};
---
+++ src/models/dto/GetPicsumPhotoAPI.dto.ts
@@ -0,0 +1,8 @@
+export interface GetPicsumPhotoAPIResponseDTO {
+  id: string;
+  author: string;
+  width: number;
+  height: number;
+  url: string;
+  download_url: string;
+}
